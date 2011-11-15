bbd21d543493e7b836cc36567cf8d03340162a5b
Vladimir Pouzanov <farcaller@gmail.com>
2011-11-15 14:21:31 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photodev::Application.config.secret_token = '4550de16277bb165915af9a28e3d0d9a121792e68a350786567d9f362637f9f201aff7c0e6b8efe2aba9bd8bf67e2a0a724a292bcdefafd6cc37518741f08756'
