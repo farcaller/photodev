2661287c9cd1f8988744bec529ac7a23fc906e8d
Vladimir Pouzanov <farcaller@gmail.com>
2011-12-08 14:57:49 +0200

    Filter browser#show by passed in collection id

--- app/controllers/browser_controller.rb
+++ app/controllers/browser_controller.rb
@@ -1,8 +1,7 @@
 class BrowserController < ApplicationController
   def show
-    @photo_in_collection = PhotoInCollection.find(params[:uuid])
+    @photo_in_collection = PhotoInCollection.where(:collection_id => params[:collection_id]).find(params[:uuid])
     # TODO auth
-    # TODO check if collection_id matches
     @photo = @photo_in_collection.photo
   end
 
