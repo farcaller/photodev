201bf1a8305c08e318a5455cd8bd190163938c8c
Vladimir Pouzanov <farcaller@gmail.com>
2011-12-08 15:14:20 +0200

    Added rescues for http 404 and 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,18 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to new_user_session_url, :alert => exception.message
   end
+  
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+    rescue_from Exception, :with => :render_500
+  end
+
+  private
+  def render_404
+    render :template => 'error_pages/404', :layout => true, :status => :not_found
+  end
+  
+  def render_500
+    render :template => 'error_pages/404', :layout => true, :status => :internal_server_error
+  end
 end
