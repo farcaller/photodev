896d7cdfc7c182b3bbfab6a172b4f6e0f84412a2
Vladimir Pouzanov <farcaller@gmail.com>
2011-12-11 19:31:29 +0200

    New different cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,51 +1,139 @@
-set :user, 'photodev'
-set :domain, 'photodev.org'
-set :application, 'photodev'
+require "bundler/capistrano"
 
-# adjust if you are using RVM, remove if you are not
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-require "rvm/capistrano"
-set :rvm_ruby_string, '1.9.3'
-set :rvm_type, :user
+set :scm,             :git
+set :repository,      "git@github.com:hackndev/photodev.git"
+set :branch,          "origin/master"
+set :migrate_target,  :current
+set :ssh_options,     { :forward_agent => true }
+set :rails_env,       "production"
+set :deploy_to,       "/home/photodev/photodev"
+set :normalize_asset_timestamps, false
 
-set :repository,  "git@github.com:havkndev/photodev" 
-set :deploy_to, "/home/#{user}/#{domain}" 
+set :user,            "photodev"
+set :group,           "photodev"
+set :use_sudo,        false
 
-role :app, domain
-role :web, domain
-role :db, domain, :primary => true
+set :domain,  "photodev.org"
 
-# you might need to set this if you aren't seeing password prompts
-# default_run_options[:pty] = true
-# As Capistrano executes in a non-interactive mode and therefore doesn't cause
-# any of your shell profile scripts to be run, the following might be needed
-# if (for example) you have locally installed gems or applications.  Note:
-# this needs to contain the full values for the variables set, not simply
-# the deltas.
-# default_environment['PATH']='<your paths>:/usr/local/bin:/usr/bin:/bin'
-# default_environment['GEM_PATH']='<your paths>:/usr/lib/ruby/gems/1.8'
-# miscellaneous options
-set :deploy_via, :remote_cache
-set :scm, 'git'
-set :branch, 'master'
-set :scm_verbose, true
-set :use_sudo, false
-set :rails_env, :production
+role :web,    domain
+role :app,    domain
+role :db,     domain, :primary => true
+
+set(:latest_release)  { fetch(:current_path) }
+set(:release_path)    { fetch(:current_path) }
+set(:current_release) { fetch(:current_path) }
+
+set(:current_revision)  { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:latest_revision)   { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip }
+
+default_environment["RAILS_ENV"] = 'production'
+
+default_environment["PATH"]         = "/home/photodev/.rvm/gems/ruby-1.9.3-p0@rails3/bin:/home/photodev/.rvm/gems/ruby-1.9.3-p0@global/bin:/home/photodev/.rvm/rubies/ruby-1.9.3-p0/bin:/home/photodev/.rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
+default_environment["GEM_HOME"]     = "/home/photodev/.rvm/gems/ruby-1.9.3-p0@rails3"
+default_environment["GEM_PATH"]     = "/home/photodev/.rvm/gems/ruby-1.9.3-p0@rails3:/home/photodev/.rvm/gems/ruby-1.9.3-p0@global"
+default_environment["RUBY_VERSION"] = "ruby-1.9.3-p0"
+
+default_run_options[:shell] = 'bash'
 
 namespace :deploy do
-  desc "cause Passenger to initiate a restart"
-  task :restart do
-    run "touch #{current_path}/tmp/restart.txt" 
+  desc "Deploy your application"
+  task :default do
+    update
+    restart
   end
 
-  desc "reload the database with seed data"
-  task :seed do
-    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+  desc "Setup your git-based deployment app"
+  task :setup, :except => { :no_release => true } do
+    dirs = [deploy_to, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+    run "git clone #{repository} #{current_path}"
+  end
+
+  task :cold do
+    update
+    migrate
+  end
+
+  task :update do
+    transaction do
+      update_code
+    end
+  end
+
+  desc "Update the deployed code."
+  task :update_code, :except => { :no_release => true } do
+    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+    finalize_update
+  end
+
+  desc "Update the database (overwritten to avoid symlink)"
+  task :migrations do
+    transaction do
+      update_code
+    end
+    migrate
+    restart
+  end
+
+  task :finalize_update, :except => { :no_release => true } do
+    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+
+    # mkdir -p is making sure that the directories are there for some SCM's that don't
+    # save empty folders
+    run <<-CMD
+      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+      mkdir -p #{latest_release}/public &&
+      mkdir -p #{latest_release}/tmp &&
+      ln -s #{shared_path}/log #{latest_release}/log &&
+      ln -s #{shared_path}/system #{latest_release}/public/system &&
+      ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
+      ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
+    CMD
+
+    if fetch(:normalize_asset_timestamps, true)
+      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
+      asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
+      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
+    end
+  end
+
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "kill -s USR2 `cat /tmp/unicorn-photodev.pid`"
+  end
+
+  desc "Start unicorn"
+  task :start, :except => { :no_release => true } do
+    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
+
+  desc "Stop unicorn"
+  task :stop, :except => { :no_release => true } do
+    run "kill -s QUIT `cat /tmp/unicorn-photodev.pid`"
+  end  
+
+  namespace :rollback do
+    desc "Moves the repo back to the previous version of HEAD"
+    task :repo, :except => { :no_release => true } do
+      set :branch, "HEAD@{1}"
+      deploy.default
+    end
+
+    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
+    task :cleanup, :except => { :no_release => true } do
+      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+    end
+
+    desc "Rolls back to the previously deployed version."
+    task :default do
+      rollback.repo
+      rollback.cleanup
+    end
   end
 end
 
-after "deploy:update_code", :bundle_install
-desc "install the necessary prerequisites"
-task :bundle_install, :roles => :app do
-  run "cd #{release_path} && bundle install"
+def run_rake(cmd)
+  run "cd #{current_path}; #{rake} #{cmd}"
 end
